aec2aa653aa0fedba2ebfe964315c9fca3143188
Matheus Caetano <matheuseprofissional@gmail.com>
2024-12-08 20:07:07 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/matheuscaet/go-cron
+
+go 1.23.4
+
+require github.com/robfig/cron v1.2.0 // indirect
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "time"
+
+    "github.com/robfig/cron"
+)
+
+func main() {
+    c := cron.New()
+
+    // Define the Cron job schedule
+    c.AddFunc("* * * * *", func() {
+        fmt.Println("Hello world!")
+    })
+
+    // Start the Cron job scheduler
+    c.Start()
+
+    // Wait for the Cron job to run
+    time.Sleep(5 * time.Minute)
+
+    // Stop the Cron job scheduler
+    c.Stop()
+}
